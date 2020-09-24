d199d8b0ee1608b82c4a5e7e0373c4ff2cd4b3a5
Ali Ibrahim <ai9sk@virginia.edu>
2020-09-24 17:38:04 -0400

    added and designed homepage at /virtualstudybuddy

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    #Made a type here! Shouldn't affect anything
+    path("virtualstudybuddy/", include("virtualstuddybuddy.urls")),
     path('admin/', admin.site.urls),
 ]
