4101a8040ac84bff1aa818f6c6df6d7897cec4d5
Aaron Ponnraj <Angelarajesh@Aarons-MBP.home>
2020-10-15 16:20:36 -0400

    Profile Feature

--- virtualstuddybuddy/models.py
+++ virtualstuddybuddy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+	username = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	name = models.CharField(max_length=50)
+	gender = models.CharField(max_length=200)
+	major = models.CharField(max_length=200)
+	age = models.IntegerField(max_length=2)
+	description = models.CharField(max_length=200)
+	picture = models.ImageField(upload_to='uploads/')
+
