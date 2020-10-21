f4b9e39bdbc038b6e67b2289812b1333b9eeae37
Aaron Ponnraj <Angelarajesh@aarons-mbp.myfiosgateway.com>
2020-10-21 18:59:01 -0400

    Unit testing

--- virtualstuddybuddy/tests.py
+++ virtualstuddybuddy/tests.py
@@ -1,15 +1,70 @@
-from django.test import TestCase
-
-
+from django.test import TestCase, RequestFactory
+from django.db import DataError
+from django.urls import reverse
 from virtualstuddybuddy.models import Profile
 # Create your tests here.
 
 class ProfileTestCases(TestCase):
 	def setUp(self):
 		Profile.objects.create() #pk=1, default
+		Profile.objects.create(username="Case1") #pk=2, invalid input
+		Profile.objects.create(name="This_input_is_too_long_for_the_field_This_input_is_too_long_for_the_field_This_input_is_too_long_for_the_field_This_input_is_too_long_for_the_field") #pk=3, invalid input
+		Profile.objects.create(name="") #pk=4, second default
 
-		Profile.objects.create(age="Invalid Input") #pk=2, invalid input
-		Profile.objects.create(username="This_input_is_too_long_for_the_field") #pk=2, invalid input
+	## Equivalence Tests
+	def test_equivalence(self):
+		default = Profile.objects.get(pk=1)
+		newProf = Profile()
+		self.assertEqual(default.__str__(), newProf.__str__())
 
-	def test_invalid_input(self):
-		exception = Profile.objects.get(pk=1)
+	def test_non_equivalence(self):
+		default = Profile.objects.get(pk=1)
+		case_1 = Profile.objects.get(pk=2)
+		self.assertNotEqual(default.__str__(), case_1.__str__())
+
+	## Boundary Tests
+	def test_invalid_name(self):
+		name = Profile.objects.get(pk=3)
+		self.assertEqual("This_input_is_too_long_for_the_field_This_input_is_too_long_for_the_field_This_input_is_too_long_for_the_field_This_input_is_too_long_for_the_field", name.name)
+		# Name defaults if there is an invalid entry
+
+	def test_invalid_gender(self):
+		name = Profile.objects.get(pk=4)
+		self.assertEqual("", name.name)
+
+	## Exception Tests
+	def test_exception_age(self):
+		with self.assertRaises(ValueError):
+			Profile.objects.create(age="invalid")
+
+
+class ProfileViewTestCases(TestCase):
+	def setUp(self):
+		Profile.objects.create() #pk=1, default
+
+	## Test Signup Template/URL
+	def test_signup(self):
+		response = self.client.post(reverse('signup'))
+		self.assertEqual(response.status_code, 200)	
+	
+	def test_signup_template(self):
+		response = self.client.post(reverse('signup'))
+		self.assertTemplateUsed(response, 'virtualstuddybuddy/signup.html')
+
+	## Test Main Page Template/URL
+	def test_main_page(self):
+		response = self.client.post(reverse('index'))
+		self.assertEqual(response.status_code, 200)
+	
+	def test_main_page_template(self):
+		response = self.client.post(reverse('index'))
+		self.assertTemplateUsed(response, 'virtualstuddybuddy/index.html')
+
+	## Test Profile Template/URL
+	def test_profile(self):
+		default = Profile.objects.get(pk=1)
+		response = self.client.get('http://caamcomputing.herokuapp.com/virtualstudybuddy/profile/1/')
+		self.assertEqual(response.status_code, 200)
+	def test_profile_template(self):
+		response = self.client.get('http://caamcomputing.herokuapp.com/virtualstudybuddy/profile/1/')
+		self.assertTemplateUsed(response, 'virtualstuddybuddy/profile.html')
--- virtualstuddybuddy/views.py
+++ virtualstuddybuddy/views.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from django.views.decorators.csrf import csrf_protect
 from django.views.generic.list import ListView
 from django.http import HttpRequest
+from django.db import DataError
 
 # Create your views here.
 
@@ -16,16 +17,22 @@ class ProfileView(generic.DetailView):
 	model = Profile
 	template_name = 'virtualstuddybuddy/profile.html'
 
+
 def index(request):
     return render(request, 'virtualstuddybuddy/index.html')
 
 def signup(request): #How we handle signups and logins
 	try:
-		if request.method == 'POST': 								#If a person filled out the sign up form
+		if request.method == 'POST': #If a person filled out the sign up form
 			profile1 = request.POST
 			#print(comment1)
 			p = Profile(username=request.user.get_username(), name=profile1['name'], gender=profile1['gender'], major=profile1['major'], age=profile1['age'], description=profile1['description'])
 			
+			if p.name == '' or p.gender == '' or p.major == '' or p.age == '' or p.description == '':
+				raise DataError(
+					"Invalid Input"
+					)
+
 			if p.is_valid():
 				p.save()
 				qs = Profile.objects.all()
