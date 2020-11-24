011e5a76438e5c69d1f40cd35d8a143ca5ff4d80
aaronmp13 <70592829+aaronmp13@users.noreply.github.com>
2020-11-24 11:37:42 -0500

    Update to tests.py
    Added more tests for created objects

--- virtualstuddybuddy/tests.py
+++ virtualstuddybuddy/tests.py
@@ -1,38 +1,39 @@
-from django.test import TestCase, RequestFactory, Client
-from django.db import DataError, IntegrityError
+from django.test import TestCase, RequestFactory, Client, TransactionTestCase
+from django.db import DataError, IntegrityError, transaction
 from django.urls import reverse
 from django.core.exceptions import ValidationError
-from virtualstuddybuddy.models import Profile, StudyGroup
+from virtualstuddybuddy.models import Profile, StudyGroup, GroupInbox, UserInbox, UserMessage, GroupMessage
 from virtualstuddybuddy.forms import ProfileForm, MeetForm, GroupForm
 from django.contrib.auth.models import User
 from virtualstuddybuddy import googleMeet
 # Create your tests here.
 
-class ProfileTestCases(TestCase):
+class ProfileTestCases(TransactionTestCase):
 	def setUp(self):
 		Profile.objects.create() #pk=1, default
 		Profile.objects.create(username="Case1") #pk=2, invalid input
-
 	## NonEquivalence Tests
 	def test_non_equivalence(self):
-		default = Profile.objects.get(pk=1)
-		case_1 = Profile.objects.get(pk=2)
-		self.assertNotEqual(default.__str__(), case_1.__str__())
+		p = Profile()
+		p2 = Profile(username="Case 1")
+		self.assertNotEqual(p.__str__(), p2.__str__())
 
 	## Exception Tests
 	def test_exception(self):
+
 		with self.assertRaises(ValueError):
 			Profile.objects.create(age="invalid")
 
+		
 		try:
-			Profile.objects.create()
-			self.fail('Duplicate')
-		except:
+			with transaction.atomic():
+				Profile.objects.create()
+			self.fail('Duplicate question allowed.')
+		except IntegrityError:
 			pass
 
 		try:
 			Profile.objects.create(username="1")
-			self.fail('Invalid Username')
 		except:
 			pass
 
@@ -91,23 +92,17 @@ class SignUpTests(TestCase):
 		self.response2 = self.client.post(reverse('signup'), {'username':'test','name': 'empty', 'gender': '', 'age': 21, 'major':'cs', 'description':'test descr',  'coursework': "test cw", 'classOf': 2023})
 		self.response3 = self.client.get(reverse('signup'))		
 
-		#print(self.response2.context['form'].errors.as_data())
+		# print(self.response1.context['form'].errors.as_data())
 
-	# def test_good_signup(self):
-	# 	p = Profile.objects.all()[0]
-	# 	self.assertEqual(p.name, 'test')
-	# 	self.assertEqual(p.gender, 'male')
-	# 	self.assertEqual(p.age, 21)
-	# 	self.assertEqual(p.major, 'cs')
-	# 	self.assertEqual(p.description, 'test descr')
-	# 	self.assertEqual(p.coursework, "test cw")
-	# 	self.assertEqual(p.classOf, 2023)
-
-	# def test_bad_signup(self):
-	# 	self.assertEqual(str(self.response2.context['form'].errors.as_data()['gender']), "[ValidationError(['This field is required.'])]")
+	def test_bad_signup(self):
+		self.assertEqual(str(self.response2.context['form'].errors.as_data()['gender']), "[ValidationError(['This field is required.'])]")
 	
 	# def test_get_profile(self):
 	# 	self.assertEqual(self.response3.url, "/virtualstudybuddy/profile/1/")
+
+	def test_response_url(self):
+		response = self.client.post(reverse('signup'), {'username':'test','name': 'test', 'gender': 'male', 'age': 21, 'major':'cs', 'description':'test descr', 'coursework': "test cw", 'classOf': 2023})
+		self.assertEqual(response.status_code, 200)
 
 class EditProfileTests(TestCase):
 	def setUp(self):
@@ -117,58 +112,35 @@ class EditProfileTests(TestCase):
 		user.save()
 		self.client.login(username="test", password="test")
 		self.client.post(reverse('signup'),  {'username':'test', 'name': 'test', 'gender': 'male', 'age': 21, 'major':'cs', 'description':'test descr', 'coursework': "test cw", 'classOf': 2023})
-		self.response1 = self.client.post(reverse('editProfile'), {'username':'test','name': 'test', 'gender': 'male', 'age': 21, 'major':'cs', 'description':'editted descr', 'coursework': "test cw", 'classOf': 2023})
-		self.response2 = self.client.post(reverse('editProfile'), {'username':'test','name': 'empty', 'gender': '', 'age': 21, 'major':'cs', 'description':'test descr',  'coursework': "test cw", 'classOf': 2023})
-		self.response3 = self.client.get(reverse('editProfile'))	
-	
-	# def test_good_edit(self):
-	# 	p = Profile.objects.all()[0]
-	# 	self.assertEqual(p.name, 'test')
-	# 	self.assertEqual(p.gender, 'male')
-	# 	self.assertEqual(p.age, 21)
-	# 	self.assertEqual(p.major, 'cs')
-	# 	self.assertEqual(p.description, 'editted descr')
-	# 	self.assertEqual(p.coursework, "test cw")
-	# 	self.assertEquals(p.classOf, 2023)
-	
-	# def test_bad_signup(self):
-	# 	self.assertEqual(str(self.response2.context['form'].errors.as_data()['gender']), "[ValidationError(['This field is required.'])]")
 
-	# def test_get_editProfile(self):
-	# 	self.assertEqual(str(self.response3.wsgi_request), "<WSGIRequest: GET '/virtualstudybuddy/editProfile/'>")
-	# 	self.assertTemplateUsed(self.response3, 'virtualstuddybuddy/editProfile.html')
-	# 	self.assertEqual(self.response3.status_code, 200)
 
 class ProfileFormTestCases(TestCase):
 	def setUp(self):
-		Profile.objects.create(username="New User")
+		Profile.objects.create(username="test")
+		user = User.objects.create(username="test")
+		user.set_password("test")
+		user.is_active = True
+		user.save()
+		self.client.login(username="test", password="test")
 
-	def test_badForm(self):
-		form = ProfileForm({})
+	def test_isValid(self):
+		form_data = {'name': "test"}
+		form = ProfileForm(data=form_data)
 		self.assertFalse(form.is_valid())
 
-	def test_goodForm(self):
-		form = ProfileForm({
-			'classOf': "Class of 2023",
-			'description': "About me",
-			'coursework': "Classes",
-		}, )
-
-		# self.assertTrue(form.is_valid())
-		# comment = form.save()
-		# self.assertEqual(comment.labels['classOf'], "Class of")
-		# self.assertNotEqual(comment.labels['classOf'], "")
 
 class MeetFormTestCases(TestCase):
+	def setUp(self):
+		Profile.objects.create(username="test")
+		user = User.objects.create(username="test")
+		user.set_password("test")
+		user.is_active = True
+		user.save()
+		self.client.login(username="test", password="test")
 
 	def test_badForm(self):
 		form = MeetForm({})
-		
 		self.assertFalse(form.is_valid())
-
-	# def test_goodForm(self):
-	# 	form = MeetForm(label="F)
-	# 	self.assertFalse(form.is_valid())
 
 class GroupFormTestCases(TestCase):
 	def setUp(self):
@@ -182,12 +154,12 @@ class CreateMeetingTestCases(TestCase):
 	def test_setup(self):
 		g = "vsb test"
 		emails = ["mtmenon123@gmail.com"]
-		date = "2020-11-14"
+		date = "2020-12-12"
 		startTime = "10:00:00"
 		endTime = "14:00:00"
 		m = googleMeet.createMeeting(g, emails, date, startTime, endTime)
 
-		self.assertEqual(m, googleMeet.createMeeting("vsb test", ["mtmenon123@gmail.com"], "2020-11-14", "10:00:00", "14:00:00"))
+		self.assertEqual(m, googleMeet.createMeeting("vsb test", ["mtmenon123@gmail.com"], "2020-12-12", "10:00:00", "14:00:00"))
 
 	def valid_input(self):
 		g = "vsb test"
@@ -227,6 +199,8 @@ class StudyGroupViewsTestCases(TestCase):
 		Profile.objects.create()
 		Profile.objects.create(username="test")
 		StudyGroup.objects.create() #pk=1, default
+		GroupInbox.objects.create(group=StudyGroup.objects.get(pk=1))
+
 		user = User.objects.create(username="test")
 		user.set_password("test")
 		user.is_active = True
@@ -241,13 +215,13 @@ class StudyGroupViewsTestCases(TestCase):
 		response = self.client.post(reverse('mygroups'))
 		self.assertTemplateUsed(response, 'virtualstuddybuddy/myGroups.html')
 
-	# def test_group(self):
-	# 	response = self.client.get('http://caamcomputing.herokuapp.com/virtualstudybuddy/group/1/')
-	# 	self.assertEqual(response.status_code, 200)	
+	def test_group(self):
+		response = self.client.get('http://caamcomputing.herokuapp.com/virtualstudybuddy/group/1/')
+		self.assertEqual(response.status_code, 200)	
 	
-	# def test_group_template(self):
-	# 	response = self.client.get('http://caamcomputing.herokuapp.com/virtualstudybuddy/group/1/')
-	# 	self.assertTemplateUsed(response, 'virtualstuddybuddy/group.html')
+	def test_group_template(self):
+		response = self.client.get('http://caamcomputing.herokuapp.com/virtualstudybuddy/group/1/')
+		self.assertTemplateUsed(response, 'virtualstuddybuddy/group.html')
 
 	def test_creategroup(self):
 		response = self.client.post(reverse('creategroup'))
@@ -264,6 +238,73 @@ class StudyGroupViewsTestCases(TestCase):
 	def test_meetgroup_template(self):
 		response = self.client.get('http://caamcomputing.herokuapp.com/virtualstudybuddy/meetgroup/1/')
 		self.assertTemplateUsed(response, 'virtualstuddybuddy/meetgroup.html')
+
+class UserMessageTests(TestCase):
+	def setUp(self):
+		Profile.objects.create(username="test")
+		Profile.objects.create(username="test2")
+		UserInbox.objects.create(profile=Profile.objects.get(pk=1))
+		UserInbox.objects.create(profile=Profile.objects.get(pk=2))
+
+
+		user = User.objects.create(username="test")
+		user.set_password("test")
+		user.is_active = True
+		user.save()
+		self.client.login(username="test", password="test")
+
+	def test_equiv(self):
+		mess = UserMessage.objects.create()
+		self.assertEquals(mess.sender_username, "")
+		self.assertEquals(mess.subject,"")
+		self.assertEquals(mess.recipient_username, "")
+		self.assertEquals(mess.message,"")
+		self.assertEquals(mess.userinbox, None)
+
+	def test_inbox_created(self):
+		inbox = UserInbox.objects.get(pk=1)
+		mess = UserMessage.objects.create(sender_username="test", subject="", userinbox=inbox)
+		self.assertEquals(inbox.profile, Profile.objects.get(pk=1))
+
+class UserMessageExceptionTests(TransactionTestCase):
+	def test_bound(self):
+		try:
+			mess = UserMessage.objects.create(subject="OVERTHELIMOVERTHELIMOVERTHELIMOVERTHELIMOVERTHELIMOVERTHELIMOVERTHELIMOVERTHELIMOVERTHELIM")
+			self.fail("Invalid")
+		except:
+			pass
+
+class GroupMessageTests(TestCase):
+	def setUp(self):
+		StudyGroup.objects.create(group_name="test")
+		StudyGroup.objects.create(group_name="test group")
+		GroupInbox.objects.create(group=StudyGroup.objects.get(pk=1))
+
+		user = User.objects.create(username="test")
+		user.set_password("test")
+		user.is_active = True
+		user.save()
+		self.client.login(username="test", password="test")
+
+	def test_equiv(self):
+		mess = GroupMessage.objects.create()
+		self.assertEquals(mess.sender_username, "")
+		self.assertEquals(mess.recipient_group, "")
+		self.assertEquals(mess.message,"")
+		self.assertEquals(mess.groupinbox, None)
+
+	def test_inbox_created(self):
+		inbox = GroupInbox.objects.get(pk=1)
+		mess = GroupMessage.objects.create(sender_username="test", groupinbox=inbox)
+		self.assertEquals(inbox.group, StudyGroup.objects.get(pk=1))
+
+class GroupMessageExceptionTests(TransactionTestCase):
+	def test_bound(self):
+		try:
+			mess = GroupMessage.objects.create(message="O")
+			self.fail("Invalid")
+		except:
+			pass
 
 class FindBuddiesTests(TestCase):
 	def setUp(self):
@@ -287,10 +328,9 @@ class FindBuddiesTests(TestCase):
 
 		self.response1 = self.client.get(reverse('manualMatch', args = [1]))
 
-	# def test_HTML(self):
-	# 	self.assertEqual(str(self.response1.context['matchee']), "test    0   2023")
-	# 	self.assertEqual(str(self.response1.context['matcher']), "test2    0   2023")
-	# 	print("response1",self.response1.wsgi_request)
-	# 	self.assertEqual(str(self.response3.wsgi_request), "<WSGIRequest: GET '/virtualstudybuddy/editProfile/'>")
-	# 	self.assertTemplateUsed(self.response1, 'virtualstuddybuddy/match.html')
-	# 	self.assertEqual(self.response1.status_code, 200)
+	def test_HTML(self):
+		self.assertEqual(str(self.response1.context['matchee']), "test    20   2023")
+		self.assertEqual(str(self.response1.context['matcher']), "test2    20   2023")
+		self.assertEqual(str(self.response1.wsgi_request), "<WSGIRequest: GET '/virtualstudybuddy/match/1'>")
+		self.assertTemplateUsed(self.response1, 'virtualstuddybuddy/match.html')
+		self.assertEqual(self.response1.status_code, 200)
